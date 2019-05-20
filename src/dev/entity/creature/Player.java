382f3f995ac23c75ff2092afc8fedbc820403cb0
QUANTUMSILVER <jeff.jc2003@gmail.com>
2019-05-20 10:25:29 +1000

    -Reset player varibles

--- src/dev/entity/creature/Player.java
+++ src/dev/entity/creature/Player.java
@@ -14,10 +14,8 @@ public class Player extends Creature{
 	public Player(Handler handler, int x, int y) {
 		super(handler, x, y);
 		health = 10;
-		speed = 40;//pixels per frame
+		speed = 4;//pixels per frame
 		handler.getCamera().focusOnEntity(this, 0);
-		hitbox.width = 0;
-		hitbox.height = 0;
 	}
 
 	private void move() {
